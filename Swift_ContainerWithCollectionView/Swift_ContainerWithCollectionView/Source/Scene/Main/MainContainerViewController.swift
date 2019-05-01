63c543f00f7d246bcc432cf898f9f0015eaabea2
h-ichiki <hi.trust.2007@gmail.com>
2019-05-01 21:16:12 +0900

    ButtonBarCollectionViewの実装

--- Swift_ContainerWithCollectionView/Swift_ContainerWithCollectionView/Source/Scene/Main/MainContainerViewController.swift
+++ Swift_ContainerWithCollectionView/Swift_ContainerWithCollectionView/Source/Scene/Main/MainContainerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainContainerViewController: UIViewController {
 
 //    @IBOutlet weak var ButtonBarViewCollectionView: UICollectionView!
     
---
+++ Swift_ContainerWithCollectionView/Swift_ContainerWithCollectionView/Source/Scene/Main/PagerTab/ButtonBarCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonBarCollectionView.swift
+//  Swift_ContainerWithCollectionView
+//
+//  Created by 一木 英希 on 2019/05/01.
+//  Copyright © 2019 一木 英希. All rights reserved.
+//
+
+import UIKit
+
+class ButtonBarCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
